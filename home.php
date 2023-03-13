952800928d9309ec0430f184ce5b07305f808252
Guilherme Handa <guihanda.pessoal@gmail.com>
2023-03-13 20:50:36 -0300

    list começo

--- home.php
+++ home.php
@@ -1,3 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chamados</title>
+    <style>
+
+        * {
+            width: 0;
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+
+        table {
+            border-collapse: separate;
+            border-spacing: 15rem 2rem;
+
+}
+     
+
+    </style>
+</head>
+<body>
+
 <?php
     session_start();
     //print_r($_SESSION);
@@ -8,25 +35,36 @@
         header('Location: login.php');
     }
     $logado = $_SESSION['loginEntrar'];
-    echo ('Seja bem vindo: ' . $logado);
-
-        
-
-
-
+    echo ('<p> Logado: '. $logado .'</p>');
 ?>
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chamadosss</title>
-</head>
-<body>
-    <center>
-        logado com sucesso! <br>
-        list dos chamados
-    </center>
+
+<center>
+    <table>
+        <div>
+            <h2>Lista de chamados</h2>
+        </div>
+        <tr>
+            <th>Nº Chamado</th>
+            <th>Tipo</th>
+            <th>Chamado</th>
+            <th>Aberto por</th>
+        </tr>
+        <tr>
+            <td>jdhawjda</td>
+            <td>ndbjawhdua</td>
+            <td>bdawndiawmdiawnd</td>
+            <td>bdawndiawmdiawnds</td>
+        </tr>
+
+    
+
+
+</center>
+
+
+
+
+
+
 </body>
 </html>
